3a429876b3ae9195e4f13d9f7427f96b93dba497
ayashiau-ga <angelicashiau@gmail.com>
2022-08-01 16:05:00 -0700

    navbar component created

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            <Link to='/' className="navbar-logo">
+                AYA <i className="fa-solid fa-face-laugh-wink"/>
+            </Link>
+            <div className='menu-icon'>
+                
+            </div>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar;
