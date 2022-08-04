1bff85810a99e966d257d0f714b278e585308e98
ayashiau-ga <angelicashiau@gmail.com>
2022-08-04 12:19:20 -0700

    hero section created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -9,7 +10,10 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path='/' exact/>
+          <Route 
+          path='/' 
+          exact 
+          element={<Home/>}/>
         </Routes>
       </Router>
     </>
